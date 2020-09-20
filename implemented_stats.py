a39a98c9db9354e1730f310ac0ef3c2a98b050b7
Gui Dalonso <guicastro.200@gmail.com>
2020-09-19 23:35:04 -0300

    organizing stats

---
+++ implemented_stats.py
@@ -0,0 +1,49 @@
+from typing import List
+
+from TeamPlayoffYear import TeamPlayoffYear
+
+def teams_that_played_the_most_elimination_games(playoff_resume: List[TeamPlayoffYear]):
+
+    # Sort all the teams with the most elimination games
+    a = {}
+    for team_seasons in playoff_resume:
+        a[
+            f"{team_seasons.year} {team_seasons.team}"
+        ] = team_seasons.get_quantity_of_elimination_games()
+    teams_with_most_elimination_games = sorted(
+        a.items(), key=lambda x: x[1], reverse=True
+    )
+    return teams_with_most_elimination_games
+
+
+def champs_that_faced_most_elimination_games(playoff_resume: List[TeamPlayoffYear]):
+    # Sort all the champs and how many elimination games they won
+    a = {}
+    for team_seasons in playoff_resume:
+        a[
+            f"{team_seasons.year} {team_seasons.team}"
+        ] = team_seasons.get_quantity_of_elimination_games_by_champ()
+
+    champs_with_most_elimination_games = sorted(
+        a.items(), key=lambda x: x[1], reverse=True
+    )
+    def filter_not_champs(item):
+        return item[1] is not False
+
+    champs_with_most_elimination_games = list(
+        filter(filter_not_champs, champs_with_most_elimination_games)
+    )
+    return champs_with_most_elimination_games
+
+
+def teams_that_won_the_most_elimination_games(playoff_resume: List[TeamPlayoffYear]):
+    # sort the teams that won most elimination games
+    a = {}
+    for team_seasons in playoff_resume:
+        a[
+            f"{team_seasons.year} {team_seasons.team}"
+        ] = team_seasons.get_quantity_of_won_elimination_games()
+    teams_that_won_most_elimination_games = sorted(
+        a.items(), key=lambda x: x[1], reverse=True
+    )
+    return teams_that_won_most_elimination_games
--- scrap.py
+++ scrap.py
@@ -1,29 +1,22 @@
-from PlayoffsYear import build_teams_resumes
-from champsGetter import retieve_nba_champs_since_merger
+from TeamPlayoffYear import build_teams_resumes
 from seasonsGetter import scrap_seasons
+from implemented_stats import teams_that_played_the_most_elimination_games, champs_that_faced_most_elimination_games, \
+    teams_that_won_the_most_elimination_games
 from seriesGetter import parse_scores_from_series_page
 import os
 
 os.environ["HUE"] = "1"
 
 if __name__ == "__main__":
-    # parse_scores_from_series_page(scrap_seasons(2018, 2019))
+
+
     all_series = parse_scores_from_series_page(scrap_seasons(1977, 1977))
     playoff_resume = build_teams_resumes(all_series)
 
-    # Sort all the teams with the most elimination games
-    a = {}
-    for team_yearly_resume in playoff_resume:
-        a[
-            f"{team_yearly_resume.year} {team_yearly_resume.team}"
-        ] = team_yearly_resume.get_quantity_of_elimination_games()
-    most_teams_with_elimination_games = sorted(
-        a.items(), key=lambda x: x[1], reverse=True
-    )
+    teams_with_most_elimination_games=teams_that_played_the_most_elimination_games(playoff_resume)
 
-    team_yearly_resume.get_quantity_of_elimination_games_by_champ()
-    team_yearly_resume.get_quantity_of_elimination_games_by_champ()
-    team_yearly_resume.get_quantity_of_won_elimination_games()
+    champs_with_most_elimination_games=champs_that_faced_most_elimination_games(playoff_resume)
 
+    teams_that_won_most_elimination_games=teams_that_won_the_most_elimination_games(playoff_resume)
 
 assert True
